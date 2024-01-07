d8ef7846c59681ee4003b0bc5077038dfe2c6f4e
spycode1 <tafodinho@gmail.com>
2024-01-07 03:23:17 +0100

    Adds fixes to deploy config

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_06_032336) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "search_logs", force: :cascade do |t|
     t.string "query"
     t.string "user_ip"
